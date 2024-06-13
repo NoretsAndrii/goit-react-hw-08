dd60fb90a81b072de1ceb19b0907a0e030c1c94e
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-06-14 02:34:23 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from '../../pages/HomePage/HomePage';
-import ContactsPage from '../../pages/ContactsPage/ContactsPage';
-import RegisterPage from '../../pages/RegisterPage/RegisterPage';
-import LoginPage from '../../pages/LoginPage/LoginPage';
 import Header from '../Header/Header';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
 import { refresh } from '../../redux/auth/operations';
 import { selectIsRefreshing } from '../../redux/auth/selectors';
 import RestrictedRoute from '../RestrictedRoute';
 import PrivateRoute from '../PrivateRoute';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const RegisterPage = lazy(() =>
+  import('../../pages/RegisterPage/RegisterPage')
+);
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
+const ContactsPage = lazy(() =>
+  import('../../pages/ContactsPage/ContactsPage')
+);
 
 function App() {
   const dispatch = useDispatch();
@@ -24,27 +29,32 @@ function App() {
   return !isRefreshing ? (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute component={<ContactsPage />} redirectTo="/login" />
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute component={<RegisterPage />} redirectTo="/" />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute component={<LoginPage />} redirectTo="/contacts" />
-          }
-        />
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute component={<ContactsPage />} redirectTo="/login" />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute component={<RegisterPage />} redirectTo="/" />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                component={<LoginPage />}
+                redirectTo="/contacts"
+              />
+            }
+          />
+        </Routes>
+      </Suspense>
     </>
   ) : null;
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,33 @@
 import { useDispatch } from 'react-redux';
+import Modal from 'react-modal';
 
 import css from './Contact.module.css';
 import { FaUser, FaPhone, FaXmark, FaWpforms } from 'react-icons/fa6';
 import { deleteContact } from '../../redux/contacts/operations';
 import { setModalOpen } from '../../redux/modal/modalSlice';
+import { useState } from 'react';
+
+Modal.setAppElement('#root');
+
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(200, 200, 200, 0.75)',
+  },
+  content: {
+    top: '30%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '0',
+    border: '2px solid black',
+    borderRadius: '8px',
+  },
+};
 
 export default function Contact({ name, number, id }) {
+  const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteContact(id));
 
@@ -23,7 +45,19 @@ export default function Contact({ name, number, id }) {
   }
   return (
     <>
-      {/* <div className={css.contactBox}> */}
+      <Modal isOpen={isModalDeleteOpen} style={customStyles}>
+        <div className={css.modal}>
+          <p>Delete contact?</p>
+          <div>
+            <button type="button" onClick={handleDelete}>
+              Yes
+            </button>
+            <button type="button" onClick={() => setIsModalDeleteOpen(false)}>
+              No
+            </button>
+          </div>
+        </div>
+      </Modal>
       <p className={css.name}>
         <FaUser className={css.icon} />
         {name}
@@ -32,12 +66,15 @@ export default function Contact({ name, number, id }) {
         <FaPhone className={css.icon} />
         {number}
       </p>
-      {/* </div> */}
       <div className={css.buttons}>
         <button className={css.button} onClick={openModal} title="Edit contact">
           <FaWpforms size={12} />
         </button>
-        <button className={css.button} onClick={handleDelete} title="Delete">
+        <button
+          className={css.button}
+          onClick={() => setIsModalDeleteOpen(true)}
+          title="Delete"
+        >
           <FaXmark size={12} />
         </button>
       </div>
